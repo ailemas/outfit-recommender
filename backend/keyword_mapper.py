ea1f1afe5caf2dc3620e68cc5d58b1e97022146a
Amelia Shen <ameliashen2006@hotmail.com>
2026-06-27 18:01:23 -0700

    Added more keywords

--- backend/keyword_mapper.py
+++ backend/keyword_mapper.py
@@ -19,16 +19,27 @@ KEYWORD_MAP: dict[str, dict[str, float]] = {
     "casual":    {"usage_Casual": 1},
     
     # gender
+    "women":     {"gender_Women": 1},
+    "woman":     {"gender_Women": 1},
+    "female":    {"gender_Women": 1},
+    "men":       {"gender_Men": 1},
+    "man":       {"gender_Men": 1},
+    "male":      {"gender_Men": 1},
+    "boys":      {"gender_Boys": 1},
+    "boy":       {"gender_Boys": 1},
+    "girls":     {"gender_Girls": 1},
+    "girl":      {"gender_Girls": 1},
+    "unisex":    {"gender_Unisex": 1},
     
     
     # colours
-    
+    "black":     {"baseColour_Black": 1},
     
     # sub-categories
-    
+    "tops":     {"subCategory_Topwear": 1},
     
     # style vibes (composite mappings)
-    "cute":    {"usage_Casual": 1,    "season_Summer": 1},
+    "cute":     {"usage_Casual": 1,    "season_Summer": 1},
     
     
 }
